2d3468b80eb62d2080a887c58d43d760f9b1b127
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-11 11:59:28 +0900

    Imp : Add show all answers available

---
+++ web/controller/answer/helper.ex
@@ -0,0 +1,19 @@
+use Croma
+
+defmodule StackoverflowCloneB.Controller.Answer.Helper do
+    use StackoverflowCloneB.Controller.Application
+
+    @collection_name "Answer"
+
+    defun collection_name() :: String.t do
+        @collection_name
+    end
+
+    def to_response_body(doc) do
+        base_map = %{
+        "id"         => doc["_id"],
+        "created_at" => doc["created_at"],
+        }
+        Map.merge(doc["data"], base_map)
+    end
+end
--- web/controller/answer/show.ex
+++ web/controller/answer/show.ex
@@ -1,7 +1,42 @@
+# use Croma
+
+# defmodule StackoverflowCloneB.Controller.Answer.ShowRequestBody do
+#   alias StackoverflowCloneB.Controller.Answer.Helper.Params
+
+#   use Croma.Struct, fields: [
+#     user_id : Croma.TypeGen.nilable(Params.Title),
+#     question_id: Croma.TypeGen.nilable(Params.Body),
+#   ]
+# end
 defmodule StackoverflowCloneB.Controller.Answer.Show do
   use StackoverflowCloneB.Controller.Application
+  # alias StackoverflowCloneB.Controller.Answer.{Helper, ShowRequestBody}
+  alias StackoverflowCloneB.Controller.Answer.Helper
+  alias StackoverflowCloneB.Error.ResourceNotFoundError
 
-  def show(_conn) do
-    # Implement me
+  # def show(%Conn{request: %Request{path_matches: %{id: id}, body: body} = conn) do
+  def show(conn) do
+    IO.inspect "ANSWER SHOW"
+    group_id = StackoverflowCloneB.Dodai.default_group_id()
+    id = conn.request.path_matches[:id]
+    root_key = StackoverflowCloneB.Dodai.root_key()
+    app_id = StackoverflowCloneB.Dodai.app_id()
+
+    # case ShowRequestBody.new(body) do {
+    #   {:error, _} -> ErrorJson.json_by_error(conn, BadRequestError.new())
+    #   {:ok, _} -> 
+    #     data = 
+    # }
+
+    request = Dodai.RetrieveDedicatedDataEntityRequest.new(group_id, Helper.collection_name(), id, root_key)
+    response = Sazabi.G2gClient.send(conn.context, app_id, request)
+    IO.inspect response
+
+    case response do
+      %Dodai.RetrieveDedicatedDataEntitySuccess{body: resp_body} 
+        -> Conn.json(conn, 200, Helper.to_response_body(resp_body))
+      %Dodai.ResourceNotFound{} 
+        -> ErrorJson.json_by_error(conn, ResourceNotFoundError.new())
+    end
   end
 end
