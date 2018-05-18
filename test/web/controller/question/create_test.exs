44baaf80524013cc7e022042f55f4552e5337386
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-18 13:07:05 +0900

    Imp : Add more test for question create

--- test/web/controller/question/create_test.exs
+++ test/web/controller/question/create_test.exs
@@ -29,4 +29,41 @@ defmodule StackoverflowCloneB.Controller.Question.CreateTest do
     assert res.status               == 200
     assert Poison.decode!(res.body) == QuestionData.gear()
   end
+
+  test "create/1 " <>
+    "when request body is invalid " <>
+    "it returns BadRequestError" do
+    :meck.expect(StackoverflowCloneB.Plug.FetchMe, :fetch, fn(conn, _) ->
+      Antikythera.Conn.assign(conn, :me, StackoverflowCloneB.TestData.UserData.dodai())
+    end)
+    invalid_bodies = [
+      %{},
+      %{"title" => "title"},
+      %{"title" => "",      "body" => "body"},
+      %{"title" => "title", "body" => ""},
+    ]
+
+    Enum.each(invalid_bodies, fn body ->
+      res = Req.post_json(@api_prefix, body, @header)
+      assert res.status               == 400
+      assert Poison.decode!(res.body) == %{
+        "code"        => "400-06",
+        "description" => "Unable to understand the request.",
+        "error"       => "BadRequest",
+      }
+    end)
+  end
+
+  test "create/1 " <>
+    "when credential is invalid or missing " <>
+    "it returns BadRequest" do
+
+    res = Req.post_json(@api_prefix, @body, @header)
+    assert res.status               == 401
+    assert Poison.decode!(res.body) == %{
+      "code"        => "401-00",
+      "description" => "The given credential is invalid.",
+      "error"       => "InvalidCredential"
+    }
+  end
 end
