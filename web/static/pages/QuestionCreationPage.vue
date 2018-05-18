c5c32b1bc98605be9f479edbb7cc3a1687e4a096
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-18 17:28:15 +0900

    Imp : Add Create Question page

--- web/static/pages/QuestionCreationPage.vue
+++ web/static/pages/QuestionCreationPage.vue
@@ -1,14 +1,66 @@
 <template>
   <div>
-    !not_implemented!
+    <div class="page-title">質問を登録する</div>
+    <form
+      class="question-form"
+      @submit.prevent="submit">
+      <div class="form-group">
+        <label for="form-title">タイトル</label>
+        <input
+          id="form-title"
+          v-model="title"
+          :maxlength="titleMaxLength"
+          class="title-edit form-control"
+          type="text"
+          minlength="1"
+          required>
+      </div>
+      <div class="form-group">
+        <label for="form-body">質問</label>
+        <textarea
+          id="form-body"
+          v-model="body"
+          :maxlength="bodyMaxLength"
+          class="body-edit form-control"
+          minlength="1"
+          required/>
+      </div>
+      <div class="form-group">
+        <button
+          class="btn btn-primary mb-2"
+          type="submit">投稿</button>
+      </div>
+    </form>
+    <hr>
+    <router-link :to="{ name: 'QuestionListPage'}">
+      一覧に戻る
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'QuestionCreationPage',
+  data() {
+    return {
+      title: '',
+      body: '',
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.$store.state.id);
+      this.$store.dispatch('createQuestion', { title: this.title, body: this.body })
+        .then(() => {
+          this.$router.push({ path: '/' });
+        });
+    },
+  },
 };
 </script>
 
 <style scoped>
+.body-edit {
+  height: 200px;
+}
 </style>
